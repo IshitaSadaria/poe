e684421e56fe857eb1d2f13ceb14f4458a86be9d
IshitaSadaria <ishitasadaria84308@gmail.com>
2022-06-27 11:28:50 +0530

    first commit

--- src/main/webapp/WEB-INF/prime.java
+++ src/main/webapp/WEB-INF/prime.java
@@ -1,35 +1,24 @@
-import java.util.Scanner;
- 
-public class prime {
- 
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int a, b, i, j, flag;
- 
-        System.out.printf("Enter lower bound of the interval: ");
-        a = sc.nextInt(); 
-
-        System.out.printf("\nEnter upper bound of the interval: ");
-        b = sc.nextInt(); 
- 
-        System.out.printf("\nPrime numbers between %d and %d are: ", a, b);
-
-        for (i = a; i <= b; i++) {
-
-            if (i == 1 || i == 0)
-                continue;
-
-            flag = 1;
- 
-            for (j = 2; j <= i / 2; ++j) {
-                if (i % j == 0) {
-                    flag = 0;
-                    break;
-                }
-            }
-            if (flag == 1)
-                System.out.println(i);
-        }
-    }
-}
+public class prime  
+{  
+public static void main(String[] args)   
+{  
+int ct=0,n=0,i=1,j=1;  
+while(n<10)  
+{  
+j=1;  
+ct=0;  
+while(j<=i)  
+{  
+if(i%j==0)  
+ct++;  
+j++;   
+}  
+if(ct==2)  
+{  
+System.out.printf("%d ",i);  
+n++;  
+}  
+i++;  
+}  
+}  
+}  
